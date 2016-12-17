4bfef7324bb879f907d2b499484af5fae8705fab
Kirit Sælensminde <k@kirit.com>
2016-12-17 14:03:52 +0700

    Allow for fetching of the Boost.ASIO work pool thread count.

--- include/f5/threading/boost-asio.hpp
+++ include/f5/threading/boost-asio.hpp
@@ -71,6 +71,11 @@ namespace f5 {
             reactor_pool(const reactor_pool &) = delete;
             reactor_pool &operator = (const reactor_pool &) = delete;
 
+            /// Return the number of threads servicing the pool
+            std::size_t size() const {
+                return threads.size();
+            }
+
             /// Return the contained io_service instance
             boost::asio::io_service &get_io_service() {
                 return ios;
