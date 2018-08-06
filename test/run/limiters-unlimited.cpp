0e6243d54887ef5093dbcd06512fda6d3d30091f
Kirit Sælensminde <kirit@kirit.com>
2018-08-06 13:53:26 +0700

    Add a basic test for the unlimited limiter.

--- test/run/limiters-unlimited.cpp
+++ test/run/limiters-unlimited.cpp
@@ -1,9 +1,25 @@
 #include <f5/threading/limiters.hpp>
+#include <cassert>
+#include <iostream>
 
 
 int main() {
     boost::asio::io_service ios;
     f5::threading::fd::unlimited ul{ios};
-    return 0;
+    for ( std::size_t n{}; n < 100; ++n ) {
+        ul.produced(1);
+    }
+
+    std::size_t consumed{};
+    for ( std::size_t n{}; n < 100; ++n ) {
+        boost::asio::spawn(ios, [&](auto yield) {
+            consumed += ul.consume(yield);
+        });
+    }
+    ios.run();
+
+    std::cout << "Produced 100, consumed " << consumed << std::endl;
+
+    return consumed == 100u ? 0 : 1;
 }
 
