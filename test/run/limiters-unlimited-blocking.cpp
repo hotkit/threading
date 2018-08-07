0fb99da4e7870c8ab8741901db2d7ee2cf3949e3
Kirit Sælensminde <k@kirit.com>
2018-08-07 18:08:46 +0700

    Try to document the test better.

--- test/run/limiters-unlimited-blocking.cpp
+++ test/run/limiters-unlimited-blocking.cpp
@@ -9,26 +9,38 @@ int main() {
     boost::asio::io_service ios;
     f5::threading::fd::unlimited ul{ios};
     std::atomic<std::size_t> consumed{};
-    f5::threading::sync s1, s2;
     std::atomic<bool> continued{false};
 
+    /**
+        This coroutine will be paused in the middle as there won't be any
+        production for it to consume.
+     */
+    f5::threading::sync s1, s2;
     boost::asio::spawn(ios, [&](auto yield) {
         s1.done();
         consumed += ul.consume(yield);
         continued = true;
         s2.done();
     });
+    /**
+        We have to run the executor in another thread because the call
+        to `run` won't exit until the coroutine has run to completion.
+     */
     std::thread t{[&]() {
         ios.run();
     }};
 
+    /// Don't continue past here until we know the coroutine has started
     s1.wait();
     if ( continued ) {
         std::cout << "Continued flag set too early" << std::endl;
         return 2;
     }
+    /// Produce something for the coroutine to consume
     ul.produced();
+    /// Make sure the coroutine has exited
     s2.wait();
+    /// Make sure that `ios.run()` has returned
     t.join();
 
     if ( consumed != 1u ) {
